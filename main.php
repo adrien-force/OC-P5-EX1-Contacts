449735a84073f5c07b9b34bd4e5d8d2e4596a2d8
Adrien <adrienforce@icloud.com>
2024-01-23 17:30:18 +0100

    help

--- main.php
+++ main.php
@@ -7,6 +7,15 @@ while (true) {
     $line = readline("\nEntrez votre commande : ");
     echo "Vous avez saisi : $line\n";
 
+    if ($line == "help") {
+        echo "Liste des commandes :\n";
+        echo "list : liste des contacts\n";
+        echo "detail [id] : détail d'un contact\n";
+        echo "create [name] [email] [phone_number] : créer un contact\n";
+        echo "delete [id] : supprimer un contact\n";
+        echo "exit : quitter le programme\n";
+    }
+
     if ($line == "list") {
         $command = new Command();
         echo $command->list();
@@ -35,14 +44,11 @@ while (true) {
             echo "Suppression annulée.\n";
             continue;
         }
-
         if ($line2 == "o") {
             $id = $matches[1];
             $command = new Command();
             echo $command->delete($id);
         }
-        
-        
     }
 
     if ($line == "exit") {
